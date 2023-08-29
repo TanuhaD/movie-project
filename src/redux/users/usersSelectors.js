ca881d0ef9622771b84b0d5ab3fe9ee668b364e4
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-08-29 22:52:20 +0300

    usersSelectors.js

---
+++ src/redux/users/usersSelectors.js
@@ -0,0 +1,6 @@
+export const userNameSelector = (state) => state.userStore.name;
+
+export const userEmailSelector = (state) => state.userStore.email;
+
+export const userTokenSelector = (state) => state.userStore.token;
+export const userUidSelector = (state) => state.userStore.uid;
