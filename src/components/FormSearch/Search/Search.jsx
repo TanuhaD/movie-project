fcccdb80a8778ed65d840405d31c30c924f1895d
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-09-04 22:58:19 +0300

    Search.jsx

---
+++ src/components/FormSearch/Search/Search.jsx
@@ -0,0 +1,17 @@
+const Search = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={12}
+    height={12}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M5.5 9.5a4 4 0 1 0 0-8 4 4 0 0 0 0 8ZM10.5 10.5 8.325 8.325"
+    />
+  </svg>
+);
+export default Search;
