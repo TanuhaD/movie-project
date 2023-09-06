c5e51795d1ba894c0283660cdb652e798c2c3ad9
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-09-06 23:01:03 +0300

    CloseBtn.jsx

---
+++ src/components/ModalWindow/CloseBtn/CloseBtn.jsx
@@ -0,0 +1,12 @@
+const CloseBtn = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={30}
+    height={30}
+    fill="none"
+    {...props}
+  >
+    <path stroke="#000" strokeWidth={2} d="m8 8 14 14M8 22 22 8" />
+  </svg>
+);
+export default CloseBtn;
