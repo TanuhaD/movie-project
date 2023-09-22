698aca77df0954a42d55ad4b613f059ce023e0be
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-09-22 23:09:36 +0300

    Pagination.jsx

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -2,41 +2,51 @@ import PropTypes from "prop-types";
 import css from "./Pagination.module.css";
 import BackArrow from "./BackArrow";
 import NextArrow from "./NextArrow";
-const Pagination = ({ total, page, setPage, limit }) => {
-  let start = 0;
-  let end = 0;
-  const pageCount = Math.ceil(total / limit);
+import clsx from "clsx";
+
+const Pagination = ({
+  total,
+  page,
+  setPage,
+  limit,
+  pagesBetween = 3,
+  maxPagesLimit,
+}) => {
+  let pageCount = Math.ceil(total / limit);
+  if (maxPagesLimit && pageCount > maxPagesLimit) {
+    pageCount = maxPagesLimit;
+  }
+
   let startDots = false;
   let endDots = false;
-  let startPage = false;
   let endPage = false;
 
-  if (pageCount <= 3) {
-    start = 1;
-    end = pageCount;
-  } else if (page <= 2) {
-    start = 1;
-    end = 3;
-    endDots = true;
+  if (pageCount > 1) {
     endPage = true;
-  } else if (pageCount - page >= 2) {
-    startPage = true;
-    startDots = true;
-    endDots = true;
-    endPage = true;
-    start = page - 1;
-    end = page + 1;
-  } else {
-    startPage = true;
-    startDots = true;
-    start = pageCount - 2;
-    end = pageCount;
   }
 
-  const totalPage = [];
-  for (let i = start; i <= end; i += 1) {
-    totalPage.push(i);
+  const pagesBetweenArray = [];
+  if (pageCount > 2) {
+    const y = Math.floor(pagesBetween / 2);
+    let start = page - y > 2 ? page - y : 2;
+    if (start > pageCount - pagesBetween) {
+      start = pageCount - pagesBetween;
+    }
+    const end =
+      start + pagesBetween - 1 < pageCount - 1
+        ? start + pagesBetween - 1
+        : pageCount - 1;
+    if (start > 2) {
+      startDots = true;
+    }
+    if (end < pageCount - 1) {
+      endDots = true;
+    }
+    for (let i = start; i <= end; i++) {
+      pagesBetweenArray.push(i);
+    }
   }
+
   const handleBtn = (num) => {
     setPage(num);
   };
@@ -56,26 +66,35 @@ const Pagination = ({ total, page, setPage, limit }) => {
       >
         <BackArrow />
       </button>
-      {startPage && (
-        <button onClick={() => handleBtn(1)} className={css.numBtn}>
-          1
-        </button>
-      )}
+      <button
+        onClick={() => handleBtn(1)}
+        className={clsx(css.numBtn, {
+          [css.activePage]: page === 1,
+        })}
+      >
+        1
+      </button>
       {startDots && <span>...</span>}
 
-      {totalPage.map((num) => (
+      {pagesBetweenArray.map((num) => (
         <button
           key={num}
-          style={page === num ? { background: " #FF6B08 " } : {}}
           onClick={() => handleBtn(num)}
-          className={css.numBtn}
+          className={clsx(css.numBtn, {
+            [css.activePage]: page === num,
+          })}
         >
           {num}
         </button>
       ))}
       {endDots && <span>...</span>}
       {endPage && (
-        <button onClick={() => handleBtn(pageCount)} className={css.numBtn}>
+        <button
+          onClick={() => handleBtn(pageCount)}
+          className={clsx(css.numBtn, {
+            [css.activePage]: page === pageCount,
+          })}
+        >
           {pageCount}
         </button>
       )}
