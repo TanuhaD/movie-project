de8f801c5ddbf1007b828eafc2adf0193244736d
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-09-24 23:11:20 +0300

    ModalWindow.jsx

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,29 +1,36 @@
+import { addDoc, collection, deleteDoc, doc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { useDispatch, useSelector } from "react-redux";
+import firebase from "../../fireBaseConfig.js";
+import {
+  addMovieToLibrary,
+  clearModalWindowMovieInfo,
+  removeMovieFromLibrary,
+} from "../../redux/moviesSlice";
 import { getMovieId } from "../../redux/operrations";
 import {
   selectModalWindowMovieInfo,
   selectQueueIds,
   selectWatchedIds,
 } from "../../redux/selectors";
+import {
+  userTokenSelector,
+  userUidSelector,
+} from "../../redux/users/usersSelectors";
 import { genresList } from "../genresList";
 import CloseBtn from "./CloseBtn/CloseBtn";
 import css from "./ModalWindow.module.css";
-import {
-  addMovieToLibrary,
-  removeMovieFromLibrary,
-} from "../../redux/moviesSlice";
-import { addDoc, collection, deleteDoc, doc } from "firebase/firestore";
-import firebase from "../../fireBaseConfig.js";
-import { userUidSelector } from "../../redux/users/usersSelectors";
+import { onSignIn } from "../../googleAuth.js";
+import backdropImage from "../../assets/imageDefault.jpg";
 
 const modalRoot = document.querySelector("#modalPortal");
 const body = document.querySelector("body");
 
 const ModalWindow = ({ handleCloseModal, id, genre_ids }) => {
   const queueId = useSelector(selectQueueIds);
-  // const queue = useSelector(selectMoviesQueueToShow);
+  const userToken = useSelector(userTokenSelector);
+
   const uid = useSelector(userUidSelector);
   const [isInQueue, setIsInQueue] = useState(
     queueId.some((movieInfo) => {
@@ -53,10 +60,14 @@ const ModalWindow = ({ handleCloseModal, id, genre_ids }) => {
     vote_average,
     vote_count,
     poster_path,
+    release_date,
   } = useSelector(selectModalWindowMovieInfo);
 
   useEffect(() => {
     dispatch(getMovieId(id));
+    return () => {
+      dispatch(clearModalWindowMovieInfo());
+    };
   }, [dispatch, id]);
 
   const handleOverlayClick = (e) => {
@@ -119,51 +130,103 @@ const ModalWindow = ({ handleCloseModal, id, genre_ids }) => {
   };
   return createPortal(
     <div className={css.overlay} onClick={handleOverlayClick}>
-      <div className={css.modal}>
-        <CloseBtn className={css.closeBtn} onClick={handleCloseModal} />
-        <div className={css.wrraperImg}>
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
-            alt=" movie"
-            className={css.image}
-          />
-        </div>
-        <div className={css.wrapperInfo}>
-          <h2 className={css.title}>{original_title}</h2>
-          <div className={css.box}>
-            <div className={css.boxText}>
-              <p className={css.text}>Vote / Votes</p>
-              <p className={css.text}>Popularity</p>
-              <p className={css.text}>Original Title</p>
-            </div>
-            <div className={css.boxInfo}>
-              <div className={css.boxVote}>
-                <p className={css.voteAverage}>{vote_average}</p>
-                <span className={css.voteCount}> / {vote_count}</span>
+      {overview && (
+        <div className={css.modal}>
+          <CloseBtn className={css.closeBtn} onClick={handleCloseModal} />
+          <div className={css.wrraperImg}>
+            <img
+              src={
+                poster_path
+                  ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+                  : backdropImage
+              }
+              alt=" movie"
+              className={css.image}
+            />
+          </div>
+          <div className={css.wrapperInfo}>
+            <h2 className={css.title}>{original_title}</h2>
+            <div className={css.box}>
+              <div className={css.boxText}>
+                <p className={css.text}>Vote / Votes</p>
+                <p className={css.text}>Popularity</p>
+                <p className={css.text}>Original Title</p>
               </div>
-              <p className={css.infoText}>{popularity}</p>
-              <p className={css.originalTitle}>{original_title}</p>
+              <div className={css.boxInfo}>
+                <div className={css.boxVote}>
+                  <p className={css.voteAverage}>{vote_average}</p>
+                  <span className={css.voteCount}> / {vote_count}</span>
+                </div>
+                <p className={css.infoText}>{popularity}</p>
+                <p className={css.originalTitle}>{original_title}</p>
+              </div>
+            </div>
+            <div className={css.wrapperGenres}>
+              <p className={css.textGenres}>genres</p>
+              <p className={css.infoGenrse}>
+                {genre_ids.map((element) => genresList[element]).join(", ")}
+              </p>
+            </div>
+
+            <h3 className={css.titleAbout}>About</h3>
+            <p className={css.overviewText}>{overview}</p>
+            <div className={css.wrapperBtn}>
+              <button
+                disabled={!userToken}
+                className={css.btn}
+                onClick={handleClickWatched}
+              >
+                {isInWatched ? "remove from watched" : "add to watched"}
+              </button>
+
+              <button
+                disabled={!userToken}
+                className={css.btn}
+                onClick={handleClickQueue}
+              >
+                {isInQueue ? "remove from queue" : "add to queue"}
+              </button>
+            </div>
+            {!userToken && (
+              <p className={css.textSingIngWarning}>
+                Please{" "}
+                <button
+                  type="submit"
+                  className={css.signInBtn}
+                  onClick={onSignIn}
+                >
+                  Sing in
+                </button>{" "}
+                to add the movie to your library!
+              </p>
+            )}
+            <div className={css.wrapperLink}>
+              <a
+                href={`https://www.google.com/search?q=${original_title} ${release_date.slice(
+                  0,
+                  4
+                )} смотреть онлайн бесплатно`}
+                target="_blank"
+                rel="noreferrer"
+                className={css.link}
+              >
+                Watch online
+              </a>
+              <a
+                href={`https://www.google.com/search?q=${original_title} ${release_date.slice(
+                  0,
+                  4
+                )} смотреть трейлер youtube`}
+                target="_blank"
+                rel="noreferrer"
+                className={css.link}
+              >
+                Watch trailer
+              </a>
             </div>
           </div>
-          <div className={css.wrapperGenres}>
-            <p className={css.textGenres}>genres</p>
-            <p className={css.infoGenrse}>
-              {genre_ids.map((element) => genresList[element]).join(", ")}
-            </p>
-          </div>
-
-          <h3 className={css.titleAbout}>About</h3>
-          <p className={css.overviewText}>{overview}</p>
-          <div className={css.wrapperBtn}>
-            <button className={css.btn} onClick={handleClickWatched}>
-              {isInWatched ? "remove from watched" : "add to watched"}
-            </button>
-            <button className={css.btn} onClick={handleClickQueue}>
-              {isInQueue ? "remove from queue" : "add to queue"}
-            </button>
-          </div>
         </div>
-      </div>
+      )}
     </div>,
     modalRoot
   );
