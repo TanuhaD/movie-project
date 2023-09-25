99118d9c89a01fef123a7d33f308fe79fb90697d
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-09-25 23:14:40 +0300

    Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import MovieList from "../../components/MovieList/MovieList";
+import Slider from "../../components/Slider/Slider";
+
 import css from "./Home.module.css";
 
 const Home = () => {
   return (
     <div className={css.container}>
+      <Slider />
       <MovieList />
     </div>
   );
