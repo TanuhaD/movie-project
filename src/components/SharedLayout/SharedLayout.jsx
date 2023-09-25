770102b82aba6f048f2e3249898a5a957c3287fa
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-09-25 23:13:34 +0300

    SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import UpBtn from "../UpBtn/UpBtn";
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <UpBtn />
       <Footer />
     </>
   );
