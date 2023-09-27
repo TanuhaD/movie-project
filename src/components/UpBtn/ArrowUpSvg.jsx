6cc5b18db984aa5ca4fb5840824c0ad87113b0d3
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-09-27 23:17:27 +0300

    ArrowUpSvg.jsx

---
+++ src/components/UpBtn/ArrowUpSvg.jsx
@@ -0,0 +1,13 @@
+const ArrowUpSvg = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+    <title>{"Artboard-4"}</title>
+    <path
+      d="M15.707 9.707a1 1 0 0 1-1.414 0L13 8.414V18a1 1 0 0 1-2 0V8.414L9.707 9.707a1 1 0 0 1-1.414-1.414l3-3a1 1 0 0 1 1.416 0l3 3a1 1 0 0 1-.002 1.414Z"
+      data-name="Up"
+      style={{
+        fill: "#f97c4b",
+      }}
+    />
+  </svg>
+);
+export default ArrowUpSvg;
