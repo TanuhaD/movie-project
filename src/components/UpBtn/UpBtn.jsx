85e19d3f7f2f2bcf88ebd123ac05f42a0caf20a8
TanuhaD <112090814+TanuhaD@users.noreply.github.com>
2023-09-27 23:17:42 +0300

    UpBtn.jsx

---
+++ src/components/UpBtn/UpBtn.jsx
@@ -0,0 +1,37 @@
+import { throttle } from "lodash";
+import { useEffect, useRef } from "react";
+import ArrowUpSvg from "./ArrowUpSvg";
+import css from "./UpBtn.module.css";
+
+const UpBtn = () => {
+  const btnRef = useRef();
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  const addThrottledScrollEvent = throttle(() => {
+    if (window.scrollY > 1000) {
+      btnRef.current.classList.add(css.visible);
+    } else {
+      btnRef.current.classList.remove(css.visible);
+    }
+  }, 300);
+  useEffect(() => {
+    document.addEventListener("scroll", addThrottledScrollEvent);
+
+    return () => {
+      document.removeEventListener("scroll", addThrottledScrollEvent);
+    };
+  }, []);
+
+  return (
+    <button ref={btnRef} className={css.upBtn} onClick={scrollToTop}>
+      Up
+      <ArrowUpSvg className={css.arrowUpSvg} />
+    </button>
+  );
+};
+
+export default UpBtn;
